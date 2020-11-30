34e2410634dc93bd7d00840e40c95312453c3f62
Thinh Nguyen <thinh@vathes.com>
2020-11-30 12:19:23 -0600

    moved from canonical-colony-management

---
+++ djsubject/__init__.py
@@ -0,0 +1,1 @@
+from .subject import schema as subject
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+here = path.abspath(path.dirname(__file__))
+
+long_description = """"
+DataJoint Elements for Animal Management
+"""
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='elements-animal',
+    version='0.0.1',
+    description="DataJoint Elements for Animal Management",
+    long_description=long_description,
+    author='DataJoint NEURO',
+    author_email='info@vathes.com',
+    license='MIT',
+    url='https://github.com/datajoint/elements-animal',
+    keywords='neuroscience animal-management datajoint',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=requirements,
+)
