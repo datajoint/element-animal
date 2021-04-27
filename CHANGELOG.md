3d9808eb3ee49dbf148a3acbdcd1b0f48743db21
Thinh Nguyen <thinh@vathes.com>
2021-04-27 13:46:20 -0500

    add GitHub Action, add CHANGELOG, bump version: 0.1.0a1

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+Observes [Semantic Versioning](https://semver.org/spec/v2.0.0.html) standard and [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) convention.
+
+## [0.1.0a1] - 2021-04-27
+### Added 
++ Added GitHub Action release process
++ Added `subject` schema
++ Added `genotyping` schema
+
+[0.1.0a1]: https://github.com/datajoint/element-animal/releases/tag/0.1.0a1
--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 from os import path
-import sys
 
+pkg_name = [p for p in find_packages() if '.' not in p][0]
 here = path.abspath(path.dirname(__file__))
 
-long_description = """"
-DataJoint Element for Animal Management
-"""
+with open(path.join(here, 'README.md'), 'r') as f:
+    long_description = f.read()
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
+with open(path.join(here, pkg_name, 'version.py')) as f:
+    exec(f.read())
+
 setup(
-    name='element-animal',
-    version='0.0.1',
+    name=pkg_name.replace('_', '-'),
+    version=__version__,
     description="DataJoint Element for Animal Management",
     long_description=long_description,
+    long_description_content_type = 'text/markdown',
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
-    url='https://github.com/datajoint/element-animal',
+    url=f'https://github.com/datajoint/{pkg_name.replace("_", "-")}',
     keywords='neuroscience animal-management datajoint',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=requirements,
