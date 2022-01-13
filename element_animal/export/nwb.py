1659be69d7990afbe9a40c969a733414981e924e
Chris Broz <CBroz@datajoint.com>
2022-01-13 12:30:43 -0600

    PEP8 E501 line len. Version duplicate info
    - PEP8: nwb.py line length issues
    - Version: remove duplicate across __init__.py and version.py
    - Version: bump, document new nwb feature in CHANGELOG

--- element_animal/export/nwb.py
+++ element_animal/export/nwb.py
@@ -16,8 +16,11 @@ def subject_to_nwb(subject_key):
         subject_id=subject_info.pop('subject'),
         sex=subject_info.pop('sex'),
         date_of_birth=datetime.combine(subject_info.pop('subject_birth_date'),
-                                       datetime.strptime('00:00:00', '%H:%M:%S').time()),
+                                       datetime.strptime('00:00:00', '%H:%M:%S'
+                                                         ).time()),
         description=json.dumps(subject_info, default=str),
-        genotype=' x '.join((subject.Line.Allele * subject.Subject.Line & subject_key).fetch('allele')),
+        genotype=' x '.join((subject.Line.Allele * subject.Subject.Line
+                             & subject_key
+                             ).fetch('allele')),
         species=subject_info.pop('species')
     )
