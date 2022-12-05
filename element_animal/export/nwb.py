610e6878e0a619d7672f5ea59c715b652f6d4f68
CBroz1 <CBroz@DataJoint.com>
2022-12-05 11:03:17 -0600

    Add species to nwb export

--- element_animal/export/nwb.py
+++ element_animal/export/nwb.py
@@ -28,6 +28,7 @@ def subject_to_nwb(session_key: dict):
             datetime.strptime("00:00:00", "%H:%M:%S").time(),
         ),
         description=json.dumps(subject_info, default=str),
+        species=(subject.Line & subject_query).fetch("species"),
         genotype=" x ".join(
             (subject.Line.Allele * subject.Subject.Line & subject_query).fetch("allele")
         ),
