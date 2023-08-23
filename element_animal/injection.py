e78dfe461bf82de9bb77df3d9fa8cea50c0deb55
kushalbakshi <kushalbakshi@gmail.com>
2023-08-23 16:32:54 -0500

    Attempt 1: Modify for formatting

--- element_animal/injection.py
+++ element_animal/injection.py
@@ -145,7 +145,8 @@ class Injection(dj.Manual):
         VirusName (foreign key): Primary key from VirusName.
         InjectionProtocol (foreign key): Primary key from InjectionProtocol.
         titer (str): Titer of injectate at the current injection site.
-        total_volume (float): Total volume injected at the current injection site.
+        total_volume (float): Total volume injected at the current injection 
+        site.
         injection_comment (str): Comments about the virus injection. 
     """
 
